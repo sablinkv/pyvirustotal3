0d69c6e29086da5f5b4c8e64562cd9f515a59abb
sablinkv <k.v.sablin@gmail.com>
2021-03-15 16:45:30 +0500

    Added impl of VT3IPAddress

--- pyvirustotal3/vt3ip.py
+++ pyvirustotal3/vt3ip.py
@@ -1,10 +1,19 @@
 import requests
 
+from ipaddress import ip_address, IPv4Address as IPv4
 from vt3exception import VT3Exception
 from vt3base import VT3Request
 
 class VT3IPAddresses(VT3Request):
     """Class for the IP Addresses endpoints."""
+
+    @staticmethod
+    def is_valid_IPv4(ip):
+        try:
+            return type(ip_address(ip)) is IPv4
+        except ValueError:
+            return False
+    
 
     def report(self, ip, timeout=None):
         """Retrieve information about an IP address.
@@ -16,10 +25,14 @@ class VT3IPAddresses(VT3Request):
             The response from the server as a json or byte sequence.
         
         Exceptions:
+            ValueError: Invalid IPv4.
             RequestException: Response timeout from the server is exceeded or server connection error.
             VT3Exception: If the request failed.
         """
-        pass
+        if not self.is_valid_IPv4(ip):
+            raise ValueError('Invalid IPv4')
+        
+        return self._get(f'/ip_addresses/{ip}', timeout=timeout)
 
     def get_comments(self, ip, limit=10, cursor=None, timeout=None):
         """Retrieve comments for an IP address.
@@ -33,10 +46,14 @@ class VT3IPAddresses(VT3Request):
             The response from the server as a json or byte sequence.
         
         Exceptions:
+            ValueError: Invalid IPv4.
             RequestException: Response timeout from the server is exceeded or server connection error.
             VT3Exception: If the request failed.
         """
-        pass
+        if not self.is_valid_IPv4(ip):
+            raise ValueError('Invalid IPv4')
+        
+        return self._get(f'/ip_addresses/{ip}/comments', limit=str(limit), cursor=cursor, timeout=timeout)
 
     def add_comments(self, ip, text, timeout=None):
         """Add a comment to an IP address.
@@ -49,10 +66,15 @@ class VT3IPAddresses(VT3Request):
             The response from the server as a json or byte sequence.
 
         Exceptions:
+            ValueError: Invalid IPv4.
             RequestException: Response timeout from the server is exceeded or server connection error.
             VT3Exception: If the request failed.
         """
-        pass
+        if not self.is_valid_IPv4(ip):
+            raise ValueError('Invalid IPv4')
+        
+        comment = {"data": {"type": "comment", "attributes": {"text": text}}}
+        return self._post(f'/ip_addresses/{ip}/comments', json=comment, timeout=timeout)
 
     @staticmethod
     def is_valid_relationship(relationship):
@@ -78,11 +100,18 @@ class VT3IPAddresses(VT3Request):
             The response from the server as a json or byte sequence.
 
         Exceptions:
-            ValueError: Invalid relationships value.
+            ValueError: Invalid IPv4 or relationships value.
             RequestException: Response timeout from the server is exceeded or server connection error.
             VT3Exception: If the request failed.
         """
-        pass
+        if not self.is_valid_IPv4(ip):
+            raise ValueError('Invalid IPv4')
+        
+        relship = relationship.lower()
+        if not self.is_valid_relationship(relship):
+            raise ValueError('Invalid relationships value')
+
+        return self._get(f'/ip_addresses/{ip}/{relship}', limit=str(limit), cursor=cursor, timeout=timeout)
 
     def get_votes(self, ip, limit=10, cursor=None, timeout=None):
         """Retrieve votes for an IP address.
@@ -96,12 +125,16 @@ class VT3IPAddresses(VT3Request):
             The response from the server as a json or byte sequence.
         
         Exceptions:
+            ValueError: Invalid IPv4.
             RequestException: Response timeout from the server is exceeded or server connection error.
             VT3Exception: If the request failed.
         """
-        pass
+        if not self.is_valid_IPv4(ip):
+            raise ValueError('Invalid IPv4')
 
-    def add_votes(self, id, malicious=True, timeout=None):
+        return self._get(f'/ip_addresses/{ip}/votes', limit=str(limit), cursor=cursor, timeout=timeout)
+
+    def add_votes(self, ip, malicious=True, timeout=None):
         """Add a vote for a IP address.
         Args:
             ip(str): IP address.
@@ -112,7 +145,13 @@ class VT3IPAddresses(VT3Request):
             The response from the server as a json or byte sequence.
 
         Exceptions:
+            ValueError: Invalid IPv4.
             RequestException: Response timeout from the server is exceeded or server connection error.
             VT3Exception: If the request failed.
         """
-        pass
+        if not self.is_valid_IPv4(ip):
+            raise ValueError('Invalid IPv4')
+
+        verdict = 'malicious' if malicious else 'harmless'
+        votes = {"data": {"type": "vote", "attributes": {"verdict": verdict}}}
+        return self._post(f'/ip_addresses/{ip}/votes', json=votes, timeout=timeout)
